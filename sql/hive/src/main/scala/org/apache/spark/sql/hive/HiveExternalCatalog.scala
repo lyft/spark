64bd83ee775506a84eb07f64d370754ad30a8c87
Catalin Toda <ctoda@lyft.com>
2022-01-24 13:28:27 -0800

    Revert "[SPARK-17658][SQL] Disallow Users to Change Table Type"
    This reverts commit 0a8e51a5e4cfd3275eff12e9fbbeb3fb487990aa.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveExternalCatalog.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveExternalCatalog.scala
@@ -139,11 +139,6 @@ private[spark] class HiveExternalCatalog(conf: SparkConf, hadoopConf: Configurat
         s"as table property keys may not start with '$SPARK_SQL_PREFIX': " +
         invalidKeys.mkString("[", ", ", "]"))
     }
-    // External users are not allowed to set/switch the table type. In Hive metastore, the table
-    // type can be switched by changing the value of a case-sensitive table property `EXTERNAL`.
-    if (table.properties.contains("EXTERNAL")) {
-      throw new AnalysisException("Cannot set or change the preserved property key: 'EXTERNAL'")
-    }
   }
 
   /**
